56ad0b57450cf00a25ae54405e187792abd40a56
batireedui <batireeduiotgonsukh@gmail.com>
2024-09-24 09:37:26 +0800

    z

--- backend/src/controllers/auth-controller.ts
+++ backend/src/controllers/auth-controller.ts
@@ -5,7 +5,31 @@ export const login = (req: Request, res: Response) => {
   // res.status(200).json({ message: "success" });
   // const { email, password } = req.body;
 };
+// const signIn = async (req, res) => {
+//   const { email, password } = req.body;
+//   const [user] = await sql`
+//     SELECT * FROM users WHERE email=${email}
+//   `;
 
+//   if (!user) {
+//     res.status(404).json({ message: "Бүртгэлтэй хэрэглэгч олдсонгүй" });
+//   } else {
+//     const isCheck = bcrypt.compareSync(password, user.password);
+//     if (!isCheck) {
+//       res.status(400).json({
+//         message: "Хэрэглэгчийн имэйл эсвэл нууц үг тохирохгүй байна.",
+//       });
+//     } else {
+//       const token = jwt.sign({ id: user.id }, "JWT_TOKEN_PASS@123", {
+//         expiresIn: "1h",
+//       });
+//       res.status(200).json({
+//         message: "success",
+//         token,
+//       });
+//     }
+//   }
+// };
 export const signup = async (req: Request, res: Response) => {
   try {
     const { firstname, lastname, email, password } = req.body;
