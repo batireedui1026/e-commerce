e759ede78e86584f332ecf741ac21c0755f68f47
batireedui <batireeduiotgonsukh@gmail.com>
2024-09-25 09:14:22 +0800

    zul

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import authRoute from "./routes/auth-route";
 import { connectDB } from "./config/db";
 import cors from "cors";
-import { Resend } from "resend";
+// import { Resend } from "resend";
 import GenerateHtml from "./utils/generateHtmlTemplate";
 
 dotenv.config();
@@ -13,28 +13,28 @@ const PORT = process.env.PORT || "";
 const MONGO_URI = process.env.MONGO_URI || "";
 // express application obeject ussgeh
 const app = express();
-const resend = new Resend(process.env.reSend);
+// const resend = new Resend(process.env.reSend);
 // middlewares
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/auth", authRoute);
 
-app.get("/", async (req, res) => {
-  const rndOtp = Math.floor(Math.random() * 10_000)
-    .toString()
-    .padStart(4, "0");
-  const { data, error } = await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
-    to: ["batireeduiotgonsukh@gmail.com"],
-    subject: "Cайн байна уу?",
-    html: GenerateHtml(),
-  });
-  if (error) {
-    console.log("email code sent error", { error });
-  }
-  res.send("welcome to api server");
-});
+// app.get("/", async (req, res) => {
+//   const rndOtp = Math.floor(Math.random() * 10_000)
+//     .toString()
+//     .padStart(4, "0");
+//   const { data, error } = await resend.emails.send({
+//     from: "Acme <onboarding@resend.dev>",
+//     to: ["batireeduiotgonsukh@gmail.com"],
+//     subject: "Cайн байна уу?",
+//     html: GenerateHtml(),
+//   });
+//   if (error) {
+//     console.log("email code sent error", { error });
+//   }
+//   res.send("welcome to api server");
+// });
 console.log("MU", MONGO_URI);
 connectDB(MONGO_URI);
 app.listen(PORT, () => {
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,6 +1,35 @@
-"use client";
+"use client"
+import axios from "axios";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
 const Login = () => {
+  const router = useRouter();
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+  const LogIn = async() =>{
+    const {email, password} =userData;
+    try{
+      const response = await axios.post(`http://localhost:8000/api/v1/auth/login`, {
+        email,
+        password,
+      });
+      console.log("response", response)
+      if(response.status === 200){
+        toast.success("User successfully signed in", {autoClose: 1000});
+        const {token} = response.data;
+        localStorage.setItem("token", token);
+        router.push("/home")
+      }
+    }catch(error){
+      console.log("there was a error signing in", error);
+      toast.error("failed to sign in. please try again");
+    }
+
+  };
   return (
     <div className="pt-52 pb-80 bg-gray-100 ">
       <h1 className="text-3xl font-semibold text-center">Нэвтрэх</h1>
@@ -8,15 +37,20 @@ const Login = () => {
         <input
           className="btn border rounded-xl w-60 flex mb-4 mt-6 pl-2 h-9 "
           placeholder="Имэйл хаяг"
-          type="password"
+          type="text" onChange={(e)=> 
+            setUserData({...userData, email: e.target.value})}
         ></input>
         <input
           className="btn border rounded-xl w-60 flex mb-4 pl-2 h-9"
           placeholder="Нууц үг"
-          type="password"
+          type="password" onChange={(e)=>
+            setUserData
+            ({...userData, password: e.target.value})
+          }
+          
         ></input>
-        <button className="btn border rounded-xl w-60 flex bg-blue-700 h-9 pl-20 mb-3 text-white transform transition-transform duration-300 hover:scale-110 ">
-          Нэвтрэх
+        <button className="btn border rounded-xl w-60 flex bg-blue-700 h-9 pl-20 mb-3 text-white transform transition-transform duration-300 hover:scale-110 " onClick={LogIn}>
+          Нэвтрэх 
         </button>
         <Link
           className="border-b-[1px] w-36 pl-4 pt-9 ml-[45px]  hover:scale-110"
