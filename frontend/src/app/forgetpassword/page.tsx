2047b79036cbe87d9aa06b8510b8b7b7bd1874d6
batireedui <batireeduiotgonsukh@gmail.com>
2024-09-27 09:35:00 +0800

    last

--- frontend/src/app/forgetpassword/page.tsx
+++ frontend/src/app/forgetpassword/page.tsx
@@ -1,6 +1,34 @@
+"use client"
+import axios from "axios";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
+import { toast } from "react-toastify";
 
 const ForgetPassword = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [step, setStep] = useState(1);
+
+  const handleEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value)
+
+  }
+
+
+  const handleSendOpt = async() => {
+    console.log(email);
+    try{
+      const res = await axios.post(`http://localhost:8000/api/v1/auth/forget-password`, {email});
+      if(res.status === 200){
+
+      }
+
+    }catch(error){
+      toast.error("Имэйл илгээхэд алдаа гарлаа")
+    }
+   
+  }
   return (
     <div className="pt-52 pb-80 bg-gray-100">
       <h1 className="text-3xl font-semibold text-center">Бүртгүүлэх</h1>
@@ -8,7 +36,7 @@ const ForgetPassword = () => {
         <input
           className="btn border rounded-xl w-60 flex mb-4 mt-6 pl-2 h-9 "
           placeholder="Имэйл хаяг оруулах"
-          type="text"
+          type="text" onChange={handleEmail}
         ></input>
         <Link
           href="/opt"
