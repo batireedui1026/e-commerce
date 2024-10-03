9af77c62f4444e83eb2b4ca7e9e4f0f793bc67d2
batireedui1026 <136432372+batireedui1026@users.noreply.github.com>
2024-10-03 12:40:28 +0800

    energy

--- backend/src/routes/product-route.ts
+++ backend/src/routes/product-route.ts
@@ -3,6 +3,6 @@ import { getAllProduct } from "../controllers/product-controller";
 
 const router = Router();
 
-router.route("/search-product").get(getAllProduct);
+router.route("/").get(getAllProduct);
 // router.route("/").post(createProduct);
 export default router;
--- frontend/src/app/detail/page.tsx
+++ frontend/src/app/detail/page.tsx
@@ -1,10 +1,61 @@
 const Detail = () => {
-    return(
-        <div>
-            <h1>
-                hello
+  const baraa = [
+    {
+      ner: "The Prompt Magazine",
+      une: 120000,
+      img: "/image.png",
+    },
+    {
+      ner: "Chunky Glyph Tee",
+      une: 120000,
+      img: "/image2.png",
+    },
+    {
+      ner: "All Smiles Nalgene",
+      une: 120000,
+      img: "/image3.png",
+    },
+    // {
+    //   ner: "Wildflower Hoodie",
+    //   une: 108000,
+    //   img: "/image4.png",
+    // },
+    {
+      ner: "Inkblot Tee",
+      une: 120000,
+      img: "/image5.png",
+    },
+    {
+      ner: "Gestures Longsleeve",
+      une: 120000,
+      img: "/image2.png",
+    },
+    {
+      ner: "Chunky Glyph Cap",
+      une: 120000,
+      img: "/image7.png",
+    },
+    {
+      ner: "Local Styles Crewneck",
+      une: 120000,
+      img: "/image8.png",
+    },
+    {
+      ner: "Chunky Glyph Cap",
+      une: 120000,
+      img: "/image7.png",
+    },
+    {
+      ner: "Doodle Hoodie",
+      une: 120000,
+      img: "/image10.png",
+    },
+  ];
 
-            </h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+export default Detail;
